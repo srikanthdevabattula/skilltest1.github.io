509e7402da1af682422c427da3ad7ada0497032b
srikanthdevabattula <122144374+srikanthdevabattula@users.noreply.github.com>
2023-03-01 15:03:26 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,43 @@
+let timeElement=document.querySelector('.watch .time');
+let startButton=document.getElementById("start");
+let stopButton=document.getElementById("stop");
+let resetButton=document.getElementById("reset");
+
+
+let seconds=0;
+let interval=null;
+
+startButton.onclick=function(){
+    if(interval){
+        return
+    }
+
+    interval=setInterval(function(){
+        seconds++;
+    
+        let hrs=Math.floor(seconds/ 3600);
+        
+        let mins=Math.floor((seconds -(hrs*3600))/60);
+        let secs=seconds % 60;
+    
+        if (secs<10) secs='0'+secs;
+        if (mins<10) mins='0'+mins;
+        if (hrs<10) hrs='0'+hrs;
+    
+        timeElement.innerText=hrs+":"+mins+":"+secs;
+    },1000);
+}
+
+
+stopButton.onclick=function(){
+    clearInterval(interval);
+    interval=null;
+}
+
+
+resetButton.onclick=function(){
+    clearInterval(interval);
+    interval=null;
+    seconds=0;
+    timeElement.innerText='00:00:00';
+}
